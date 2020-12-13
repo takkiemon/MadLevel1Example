8ab7cc71c4bb0ddf7f1554798c95472506533569
taktop <takkie91@hotmail.com>
2020-12-13 22:15:14 +0100

    subscribed the confirm button

--- app/src/main/java/com/example/madlevel1examplev02/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1examplev02/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.madlevel1examplev02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1examplev02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) // Sets the activity layout resource file.
+
+        // Using the id given in the layout file you can access the component.
+        // Set an action when the user clicks on the confirm button.
+        binding.btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
+    }
+
+    /**
+     * Check if the submitted answer is correct.
+     */
+    private fun checkAnswer() {
+        val answer = binding.etAnswer.text.toString()
+
+        // When the answer equals "giraffe" then display a correct message using a toast message.
+        // Otherwise display an incorrect message.
+        val message = if (answer == getString(R.string.giraffe)) {
+            getString(R.string.correct)
+        } else {
+            getString(R.string.incorrect)
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
